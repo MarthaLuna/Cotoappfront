2aa427cd729b5a8c5bd12516f6a67b9ac243ad44
moralessf <fcesar_moan@hotmail.com>
2022-06-03 14:29:50 -0500

    Correción de importación en archivo Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Home.scss";
-import Navbar from "./Navbar";
-import Main from "./Main";
-import Footer from "./Footer";
+import {Navbar} from "./Navbar";
+import {Main} from "./Main";
+import {Footer} from "./Footer";
 
 export const Home=()=>
 {
