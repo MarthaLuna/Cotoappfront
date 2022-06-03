48f827331cbbaedca5676f1c13b432caa05e2805
moralessf <fcesar_moan@hotmail.com>
2022-06-03 14:17:10 -0500

    Corrección de dirección de importaciones en archivo Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Home.scss";
-import Navbar from "./Navbar/Navbar";
-import Main from "./Main/Main";
-import Footer from "./Footer/Footer";
+import Navbar from "./Navbar";
+import Main from "./Main";
+import Footer from "./Footer";
 
 export const Home=()=>
 {
