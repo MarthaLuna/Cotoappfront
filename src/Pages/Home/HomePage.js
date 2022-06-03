05218be77e867102a5835dc9674e110352fa062f
Martha Luna <lunam91@hotmail.com>
2022-06-03 17:13:48 -0500

    cambiando codigo para arreglar rutas privadas

--- src/Pages/Home/HomePage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import {Home} from "../../Components/Home";
-const HomePage = () => {
-    return (
-        <Home/>
-    )
-}
-
-export default HomePage
--- src/Pages/Residentes/ResidentesPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const ResidentesPage = () => {
-    return (
-        <div>ResidentesPage</div>
-    )
-}
