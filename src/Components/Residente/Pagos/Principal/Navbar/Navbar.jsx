50c61fc764380d3d8bc7173037f2543723330434
moralessf <fcesar_moan@hotmail.com>
2022-06-08 21:42:42 -0500

    Prueba de vistas residente

--- src/Components/Residente/Pagos/Principal/Navbar/Navbar.jsx
+++ src/Components/Residente/Pagos/Principal/Navbar/Navbar.jsx
@@ -13,11 +13,11 @@ export const Navbar =()=>{
                     <div className="ms-4" id="Reporte-Gastos">Reportes</div>
                 </div>
                 <button type="button" className="btn btn-outline-light">
-                    <span className="d-none d-md-flex">
+                    <span className="d-none d-md-flex" style={{color:"white"}}>
                     Salir
                     <i className="ms-2 bi bi-door-open-fill"></i>
                     </span>
-                    <i className="d-md-none bi bi-door-open-fill"></i>
+                    <i className="d-md-none bi bi-door-open-fill" style={{color:"white"}}></i>
                 </button>
             </Container>
         </Navbar2>
